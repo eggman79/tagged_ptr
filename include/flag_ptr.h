d7fee8a51c194c26b765aa646a2c8cdb913af379
eggman79 <eggman79@protonmail.com>
2021-06-21 14:23:22 +0200

    added unittests

--- include/flag_ptr.h
+++ include/flag_ptr.h
@@ -1,29 +1,82 @@
 #include <cstdlib>
 #include <cstdint>
+#include <bit>
+#include <iostream>
 #include <cassert>
 #include <utility>
+#include <numeric>
+#include <tuple>
 #include <type_traits>
 
-template <typename PtrType, typename FlagsType, bool UniquePtr=true>
-class flag_ptr {
-public:
-    explicit flag_ptr(PtrType* ptr) : m_ptr(ptr) {}
+template <typename Type, size_t Size>
+struct flag {
+    using type = Type;
+    static constexpr auto size = Size;
+};
 
+template <typename...Flags>
+struct flags {
+    using type = std::tuple<Flags...>;
+};
+
+template <std::size_t Index, size_t ToIndex, typename Tuple>
+constexpr size_t sum_flag_offset() {
+    using Flag = typename std::tuple_element<Index, Tuple>::type;
+    if constexpr (Index == ToIndex)
+        return 0;
+    else {
+        return sum_flag_offset<Index + 1, ToIndex, Tuple>() + Flag::size;
+    }
+}
+
+template <typename Tuple>
+constexpr size_t get_flags_size() {
+    constexpr auto Count = std::tuple_size<Tuple>::value;
+
+    if constexpr (!Count)
+        return 0;
+    else {
+        using Flag = typename std::tuple_element<Count - 1, Tuple>::type;
+        return sum_flag_offset<0, Count - 1, Tuple>() + Flag::size;
+    }
+}
+
+template <typename PtrType, typename Flags, bool AutoDestruct=true>
+class flag_ptr {
+private:
+    using FlagPtrType = flag_ptr<PtrType, Flags, AutoDestruct>;
+
+    static constexpr std::size_t log2(size_t n) {
+        return ((n < 2) ? 0 : 1 + log2(n / 2));
+    }
+
+    template <typename Type>
+    static constexpr size_t alignment_of_in_bits() {
+        return log2(std::alignment_of<Type>::value);
+    }
+
+    static constexpr std::size_t BitfieldSize = alignment_of_in_bits<PtrType*>();
+    static constexpr std::uintptr_t FlagMask = ((0x1 << alignment_of_in_bits<PtrType*>()) - 1);
+    static constexpr std::uintptr_t PtrMask = ~FlagMask;
+
+public:
     template <typename...Args>
     explicit flag_ptr(Args&&...args) : m_ptr(new PtrType(std::forward<Args>(args)...)) {}
 
+    explicit flag_ptr(PtrType* ptr) : m_ptr(ptr) {}
+
     flag_ptr() : m_ptr(nullptr) {}
 
-    flag_ptr(const flag_ptr<PtrType, FlagsType, UniquePtr>& f) = delete;
+    flag_ptr(const FlagPtrType& f) = delete;
 
-    flag_ptr(flag_ptr<PtrType, FlagsType, UniquePtr>&& f) noexcept {
+    flag_ptr(FlagPtrType&& f) noexcept {
         m_ptr = f.m_ptr;
         f.m_ptr = nullptr;
     }
 
-    auto& operator=( const flag_ptr<PtrType, FlagsType, UniquePtr>& f) = delete;
+    auto& operator=(const FlagPtrType& f) = delete;
 
-    auto& operator=( flag_ptr<PtrType, FlagsType, UniquePtr>&& f) noexcept {
+    auto& operator=(FlagPtrType&& f) noexcept {
         m_ptr = f.m_ptr;
         f.m_ptr = nullptr;
         return *this;
@@ -31,38 +84,66 @@ public:
 
     ~flag_ptr() { delete_ptr(); }
 
-    void set_flags(FlagsType flags) {
-        const std::uintptr_t fl = *(std::uintptr_t*)&flags;
-        m_flags = fl;
+    template <size_t Index>
+    static constexpr size_t get_flag_offset() {
+        return sum_flag_offset<0, Index, typename Flags::type>();
     }
 
-    template <FlagsType flags>
-    void set_flags() {
-        const std::uintptr_t fl = *(std::uintptr_t*)&flags;
-        m_flags = fl;
+    template <size_t Index>
+    static constexpr size_t get_flag_size() {
+        using Flag = typename std::tuple_element<Index, typename Flags::type>::type;
+        return Flag::size;
     }
 
-    FlagsType get_flags() const {
-        const std::uintptr_t fl = (std::uintptr_t)m_flags;
-        return *(FlagsType*)(&fl);
+    static constexpr size_t get_flags_size() {
+        constexpr auto count = std::tuple_size<typename Flags::type>::value;
+        return get_flag_offset<count - 1>() + get_flag_size<count - 1>();
     }
 
-    const PtrType* operator->() const {
+    template <size_t Index, typename Type>
+    void set_flag(Type value) noexcept {
+        using Flag = typename std::tuple_element<
+            Index,
+            typename Flags::type>::type;
+
+        const auto val = *(uint8_t*)&value;
+        constexpr auto size = Flag::size;
+        constexpr auto offset = get_flag_offset<Index>();
+        constexpr auto value_mask = ((1 << size) - 1) << offset;
+        constexpr auto inverted_value_mask = ~value_mask;
+        m_ptr = (PtrType*)((((uintptr_t)m_ptr) & inverted_value_mask) | ((val << offset) & value_mask));
+    }
+
+    template <size_t Index>
+    auto get_flag() const noexcept {
+        using Flag = typename std::tuple_element<Index, typename Flags::type>::type;
+        constexpr auto size = Flag::size;
+        constexpr auto offset = get_flag_offset<Index>();
+        constexpr auto value_mask = ((0x1 << size) - 1);
+        const auto value = (((uintptr_t)m_ptr) >> offset) & value_mask;
+        return *(typename Flag::type*)(&value);
+    }
+
+    const PtrType* operator->() const noexcept {
         return (PtrType*)get_raw_ptr();
     }
 
-    PtrType* operator->()  {
+    PtrType* operator->() noexcept {
         return (PtrType*)get_raw_ptr();
     }
 
-    const PtrType& operator*() const {
+    const PtrType& operator*() const noexcept {
         return *(PtrType*)get_raw_ptr();
     }
 
-    PtrType& operator*() {
+    PtrType& operator*() noexcept {
         return *(PtrType*)get_raw_ptr();
     }
 
+    void delete_ptr() {
+        if constexpr(AutoDestruct)
+            delete get_raw_ptr();
+    }
     PtrType* get() {
         return (PtrType*)get_raw_ptr();
     }
@@ -80,10 +161,10 @@ public:
     }
 
     void reset_only_ptr(PtrType* ptr) {
+        constexpr auto flags_size = get_flags_size();
+        const auto flags = (uintptr_t)m_ptr & ((0x1 << flags_size) - 1);
         delete_ptr();
-        const auto prev_flags = get_flags();
-        m_ptr = ptr;
-        set_flags(prev_flags);
+        m_ptr = (PtrType*)((uintptr_t)ptr | flags);
     }
 
     void reset() {
@@ -94,21 +175,8 @@ public:
         delete_ptr();
         m_ptr = ptr;
     }
-
 private:
-    static constexpr std::size_t log2(size_t n) {
-        return ((n < 2) ? 0 : 1 + log2(n / 2));
-    }
-
-    template <typename Type>
-    static constexpr size_t alignment_of_in_bits() {
-        return log2(std::alignment_of<Type>::value);
-    }
-
-    static constexpr std::size_t BitfieldSize = alignment_of_in_bits<PtrType*>();
-    static constexpr std::uintptr_t PtrMask = ~((0x1 << alignment_of_in_bits<PtrType*>()) - 1);
-
-    union {
+      union {
         PtrType* m_ptr;
         std::uintptr_t m_flags : BitfieldSize;
     };
@@ -116,14 +184,9 @@ private:
     PtrType* get_raw_ptr() const {
         return (PtrType*)(((std::uintptr_t)m_ptr) & PtrMask);
     }
-
-    void delete_ptr() {
-        if constexpr(UniquePtr)
-            delete get_raw_ptr();
-    }
 };
 
-template <typename T, typename FlagsType, bool UniquePtr=true, typename...Args>
+/*template <typename T, typename FlagsType, bool AutoDestruct=true, typename...Args>
 auto make_flag_ptr(Args&&...args) {
-   return flag_ptr<T, FlagsType, UniquePtr>(args...);
-}
+   return flag_ptr<T, FlagsType, AutoDestruct>(args...);
+}*/
